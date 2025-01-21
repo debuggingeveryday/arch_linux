f29be8a1884dde30f2e14f0553354a51af15415c
scarfaceshing <edrianjansolatorio@gmail.com>
2025-01-21 18:17:44 +0800

    add executable 1 at the time

--- desktop-setup.sh
+++ desktop-setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # install packages
-pacman -Sy bspwm sxhkd xorg-xinit xorg-server xorg-xinit dunst ly alacritty dmenu neovim
+pacman -Sy bspwm sxhkd xorg-xinit xorg-server xorg-xinit dunst ly alacritty dmenu neovim &&
 
 # copy config
-install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc && 
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc &&
+cp /etc/X11/xinit/xinitrc ~/.xinitrc &&
 
 # enable service
-systemctl enable --now ly.service
+systemctl enable --now ly.service &&
 
 # modify
 
