064966ae0194c380e13f50ee709dbcfb684763d9
scarfaceshing <edrianjansolatorio@gmail.com>
2025-01-21 18:15:25 +0800

    desktop setup

---
+++ desktop-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# install packages
+pacman -Sy bspwm sxhkd xorg-xinit xorg-server xorg-xinit dunst ly alacritty dmenu neovim
+
+# copy config
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+# enable service
+systemctl enable --now ly.service
+
+# modify
+
+echo "
+bspwm &
+sxhkd &
+dusnt
+" >> ~/.xinitrc
